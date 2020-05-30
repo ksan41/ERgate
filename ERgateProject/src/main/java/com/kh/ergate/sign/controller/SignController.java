c95c335e9aac48197a6cc608bbdc3423ea16d478
ksan41 <58001871+ksan41@users.noreply.github.com>
2020-05-30 14:36:25 +0900

    SignController

--- ERgateProject/src/main/java/com/kh/ergate/sign/controller/SignController.java
+++ ERgateProject/src/main/java/com/kh/ergate/sign/controller/SignController.java
@@ -1,13 +1,17 @@
 package com.kh.ergate.sign.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kh.ergate.sign.model.service.SignServiceImpl;
 import com.kh.ergate.sign.model.vo.SignDocument;
 
+@Controller
 public class SignController {
 	
+	@Autowired
 	private SignServiceImpl siSevice;
 	
 	@RequestMapping("waitingList.si")
