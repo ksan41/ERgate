75e5a1a2666fa0d5b52dfde1c5dfb0d9d0d30f0e
LEEJIHYE15 <59041629+LEEJIHYE15@users.noreply.github.com>
2020-06-01 21:11:23 +0900

    회의실 관련 수정
    pom.xml 변경 됐습니다.

--- ERgateProject/src/main/java/com/kh/ergate/meetingroom/controller/MeetingroomController.java
+++ ERgateProject/src/main/java/com/kh/ergate/meetingroom/controller/MeetingroomController.java
@@ -1,18 +1,17 @@
 package com.kh.ergate.meetingroom.controller;
 
 
-import java.util.ArrayList;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.kh.ergate.common.model.vo.PageInfo;
-import com.kh.ergate.common.template.Pagination;
 import com.kh.ergate.meetingroom.model.service.MeetingroomService;
-import com.kh.ergate.meetingroom.model.vo.Meetingroom;
 
-
+//어노테이션을 통해 bean으로 등록(내부적으로 HandlerMapping이 됨)
+@Controller
 public class MeetingroomController {
 
 	@Autowired
@@ -26,18 +25,29 @@ public class MeetingroomController {
 	@RequestMapping("statusList.me")
 	public String statusList(int currentPage, Model model) {
 		
-		int listCount = mrService.statusListCount();
+		/*
+		 * int listCount = mrService.statusListCount();
+		 * 
+		 * PageInfo pi = Pagination.getPageInfo(listCount, currentPage, 10, 5);
+		 * 
+		 * ArrayList<Meetingroom> list = mrService.statusList(pi);
+		 * 
+		 * model.addAttribute("pi", pi); model.addAttribute("list", list);
+		 */
 		
-		PageInfo pi = Pagination.getPageInfo(listCount, currentPage, 10, 5);
 		
-		ArrayList<Meetingroom> list = mrService.statusList(pi);
-		
-		model.addAttribute("pi", pi);
-		model.addAttribute("list", list);
-		
-		
-		return "Meetingroom/meetingroomReservationList";
+		return "meetingroom/meetingroomReservationList";
 	}
+	
+	@RequestMapping("currentStatus.me")
+	public String currentStatus(Model model) {
+		
+		
+		
+		return "meetingroom/meetingroomCurrentStatus";
+		
+	}
+
 	
 	
 	
